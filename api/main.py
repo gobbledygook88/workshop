10bf9ee74446a4b3ce5c6bdc715f4ef0c9df67a9
Rosie O'Donnell <61861015+Rosie-ODonnell@users.noreply.github.com>
2021-12-09 16:43:04 +0000

    add in initial tests and response data

--- api/main.py
+++ api/main.py
@@ -10,4 +10,10 @@ async def root():
 
 @app.post("/game")
 async def player():
-    return {"gameId": "abc-defg-hijk"}
+    return {
+  "gameId": "abc-defg-hijk",           
+  "playerPlayed": "paper",
+  "serverPlayed": "rock",
+  "result": "you won!",               
+  "timestamp": "2021-12-01T10:10:00Z"  
+}
--- api/test_api.py
+++ api/test_api.py
@@ -16,4 +16,9 @@ def test_post_request():
     response = client.post("/game", json = {
     "playerPlayed": "paper"})
     assert response.json()["gameId"] == "abc-defg-hijk"
-     
+    assert response.json()["playerPlayed"] == "paper"
+    assert response.json()["serverPlayed"] == "rock"
+    assert response.json()["result"] == "you won!"
+    assert response.json()["timestamp"] == "2021-12-01T10:10:00Z"
+    
+
