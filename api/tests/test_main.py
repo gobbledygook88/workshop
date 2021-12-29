e3b4f2dca3317857829dd09237fbb5b36260f312
James Brignull <70067372+jamesbrignull@users.noreply.github.com>
2021-12-29 10:39:37 +0000

    Test caters for server play

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,8 +1,15 @@
-from fastapi.testclient import TestClient
+from enum import Enum
 
+from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
+
+
+class Gesture(Enum):
+    rock = "rock"
+    paper = "paper"
+    scissors = "scissors"
 
 
 def test_player_plays():
@@ -10,7 +17,6 @@ def test_player_plays():
     assert response.status_code == 200
     assert response.json()['playerPlayed'] == "paper"
     assert response.json()['gameId'] == "abc-defg-hijk"
-    assert response.json()['serverPlayed'] == "rock"
+    assert response.json()['serverPlayed'] == Gesture.rock or Gesture.paper or Gesture.scissors
     assert response.json()['result'] == "you won!"
     assert response.json()['timestamp'] == "2021-12-01T10:10:00Z"
-
