90af564a5d5f7f9ccc50cbc0e607c047ffc16792
James Brignull <70067372+jamesbrignull@users.noreply.github.com>
2021-12-29 10:34:29 +0000

    Post setup and test working

--- api/main.py
+++ api/main.py
@@ -1,9 +1,30 @@
+import random
+from enum import Enum
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Gesture(Enum):
+    rock = "rock"
+    paper = "paper"
+    scissors = "scissors"
+
+
+class Action(BaseModel):
+    playerPlayed: Gesture
+
 
 app = FastAPI()
 
 
-
-@app.post("/")
-async def player_paper():
-    return {"playerPlayed": "Paper", "gameId": "abc-defg-hijk"}
+@app.post("/play")
+async def player_plays(action: Action):
+    server_action = random.choice(['rock', 'paper', 'scissors'])
+    return {
+        "gameId": "abc-defg-hijk",
+        "playerPlayed": action.playerPlayed,
+        "serverPlayed": server_action,
+        "result": "you won!",  # determine win/loss
+        "timestamp": "2021-12-01T10:10:00Z"
+    }
--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -5,13 +5,12 @@ from main import app
 client = TestClient(app)
 
 
-def test_player_paper():
-    response = client.post("/", json={"playerPlayed": "Paper"})
+def test_player_plays():
+    response = client.post("/play", json={"playerPlayed": "paper"})
     assert response.status_code == 200
-    assert response.json()['playerPlayed'] == "Paper"
+    assert response.json()['playerPlayed'] == "paper"
     assert response.json()['gameId'] == "abc-defg-hijk"
-    assert response.json()['serverPlayed'] == "Rock"
-
-
-
+    assert response.json()['serverPlayed'] == "rock"
+    assert response.json()['result'] == "you won!"
+    assert response.json()['timestamp'] == "2021-12-01T10:10:00Z"
 
