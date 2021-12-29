17ef114e3fbc5c902bbe673c57e8bee74dbe592b
James Brignull <70067372+jamesbrignull@users.noreply.github.com>
2021-12-29 11:03:42 +0000

    Handle results

--- api/main.py
+++ api/main.py
@@ -11,6 +11,12 @@ class Gesture(Enum):
     scissors = "scissors"
 
 
+class Result(Enum):
+    win = "you win!"
+    loss = "you lose! haha"
+    draw = "draw... lame"
+
+
 class Action(BaseModel):
     playerPlayed: Gesture
 
@@ -18,13 +24,31 @@ class Action(BaseModel):
 app = FastAPI()
 
 
+def handle_result(action: Gesture, server: Gesture):
+    if action == server:
+        return Result.draw
+    if action == Gesture.rock and server == Gesture.paper:
+        return Result.loss
+    if action == Gesture.paper and server == Gesture.scissors:
+        return Result.loss
+    if action == Gesture.scissors and server == Gesture.rock:
+        return Result.loss
+    if action == Gesture.rock and server == Gesture.scissors:
+        return Result.win
+    if action == Gesture.paper and server == Gesture.rock:
+        return Result.win
+    if action == Gesture.scissors and server == Gesture.paper:
+        return Result.win
+
+
 @app.post("/play")
 async def player_plays(action: Action):
-    server_action = random.choice(['rock', 'paper', 'scissors'])
+    server_action = random.choice(list(Gesture))
+
     return {
         "gameId": "abc-defg-hijk",
         "playerPlayed": action.playerPlayed,
         "serverPlayed": server_action,
-        "result": "you won!",  # determine win/loss
+        "result": handle_result(action.playerPlayed, server_action),  # determine win/loss
         "timestamp": "2021-12-01T10:10:00Z"
     }
--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -12,11 +12,17 @@ class Gesture(Enum):
     scissors = "scissors"
 
 
+class Result(Enum):
+    win = "you win!"
+    loss = "you lose! haha"
+    draw = "draw... lame"
+
+
 def test_player_plays():
     response = client.post("/play", json={"playerPlayed": "paper"})
     assert response.status_code == 200
     assert response.json()['playerPlayed'] == "paper"
     assert response.json()['gameId'] == "abc-defg-hijk"
     assert response.json()['serverPlayed'] == Gesture.rock or Gesture.paper or Gesture.scissors
-    assert response.json()['result'] == "you won!"
+    assert response.json()['result'] == Result.win or Result.loss or Result.draw
     assert response.json()['timestamp'] == "2021-12-01T10:10:00Z"
