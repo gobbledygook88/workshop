4cb2740f78749ad48453650766295468860a3879
Rosie O'Donnell <61861015+Rosie-ODonnell@users.noreply.github.com>
2022-01-06 17:50:49 +0000

    simple game implemented with tests

--- api/main.py
+++ api/main.py
@@ -1,19 +1,55 @@
+
+   
+import random
+from enum import Enum
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Gesture(Enum):
+    rock = "rock"
+    paper = "paper"
+    scissors = "scissors"
+
+
+class Result(Enum):
+    win = "you win... hurray!"
+    loss = "you lose :("
+    draw = "it's a draw... hurray?"
+
+
+class Action(BaseModel):
+    playerPlayed: Gesture
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 @app.post("/game")
-async def player():
-    return {
+async def player_plays(action: Action):
+  server_action = random.choice(list(Gesture))
+
+  return {
   "gameId": "abc-defg-hijk",           
-  "playerPlayed": "paper",
-  "serverPlayed": "rock",
-  "result": "you won!",               
+  "playerPlayed": action.playerPlayed,
+  "serverPlayed": server_action,
+  "result": handle_result(action.playerPlayed, server_action),               
   "timestamp": "2021-12-01T10:10:00Z"  
 }
+
+def handle_result(player: Gesture, server: Gesture):
+    if player == server:
+        return Result.draw
+    if player == Gesture.rock and server == Gesture.paper:
+        return Result.loss
+    if player == Gesture.paper and server == Gesture.scissors:
+        return Result.loss
+    if player == Gesture.scissors and server == Gesture.rock:
+        return Result.loss
+    if player == Gesture.rock and server == Gesture.scissors:
+        return Result.win
+    if player == Gesture.paper and server == Gesture.rock:
+        return Result.win
+    if player == Gesture.scissors and server == Gesture.paper:
+        return Result.win
--- api/test_api.py
+++ api/test_api.py
@@ -1,24 +1,52 @@
-from main import app
-
 from fastapi.testclient import TestClient
+from main import app, Gesture, Result
 
 client = TestClient(app)
 
-def test_create_game():
-    assert 1 == 1
 
-def test_hello_world():
-    response = client.get("/")
+def test_player_plays():
+    response = client.post("/play", json={"playerPlayed": "paper"})
     assert response.status_code == 200
-    assert response.json()['message'] == 'Hello World'
+    assert response.json()['gameId'] == "abc-defg-hijk"
+    assert response.json()['playerPlayed'] == "paper"
+    assert response.json()['timestamp'] == "2021-12-01T10:10:00Z"
 
-def test_post_request():
-    response = client.post("/game", json = {
-    "playerPlayed": "paper"})
-    assert response.json()["gameId"] == "abc-defg-hijk"
-    assert response.json()["playerPlayed"] == "paper"
-    assert response.json()["serverPlayed"] == "rock"
-    assert response.json()["result"] == "you won!"
-    assert response.json()["timestamp"] == "2021-12-01T10:10:00Z"
-    
 
+def test_server_returns_allowed_result():
+    response = client.post("/play", json={"playerPlayed": "paper"})
+    assert response.json()['result'] in [result.value for result in Result]
+
+
+def test_server_result_when_player_plays_paper():
+    response = client.post("/play", json={"playerPlayed": "paper"})
+    if (response.json()['serverPlayed'] == Gesture.scissors):
+        assert response.json()['result'] == Result.win
+    if (response.json()['serverPlayed'] == Gesture.paper):
+        assert response.json()['result'] == Result.draw
+    if (response.json()['serverPlayed'] == Gesture.rock):
+        assert response.json()['result'] == Result.loss
+
+
+def test_server_result_when_player_plays_rock():
+    response = client.post("/play", json={"playerPlayed": "rock"})
+    if (response.json()['serverPlayed'] == Gesture.scissors):
+        assert response.json()['result'] == Result.win
+    if (response.json()['serverPlayed'] == Gesture.paper):
+        assert response.json()['result'] == Result.loss
+    if (response.json()['serverPlayed'] == Gesture.rock):
+        assert response.json()['result'] == Result.draw
+
+
+def test_server_result_when_player_plays_scissors():
+    response = client.post("/play", json={"playerPlayed": "scissors"})
+    if (response.json()['serverPlayed'] == Gesture.scissors):
+        assert response.json()['result'] == Result.draw
+    if (response.json()['serverPlayed'] == Gesture.paper):
+        assert response.json()['result'] == Result.win
+    if (response.json()['serverPlayed'] == Gesture.rock):
+        assert response.json()['result'] == Result.loss
+
+
+def test_server_plays_allowed_gesture():
+    response = client.post("/play", json={"playerPlayed": "paper"})
+    assert response.json()['serverPlayed'] in [play.value for play in Gesture]
